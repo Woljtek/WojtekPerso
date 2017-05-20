80b1683a576c4cad12dd392bb36e4f581ad883ec
Woljtek <fabien.craheix@gmail.com>
2017-05-20 16:53:05 +0200

    Add init GIT script

---
+++ conf/initGit.sh
@@ -0,0 +1,20 @@
+# Init: 20/05/2017
+# Author : F. CRHX
+# Synopsis :This script installs GIT and gets back my repositories
+# SSH key must be created before
+
+# Install GIT
+sudo apt install git
+
+git init
+
+# List of my repositories and clone
+REPO1="git@github.com:Woljtek/WojtekPerso.git"
+REPO2="git@github.com:Woljtek/hello-word"
+#arrayRepo=("$REPO1" "$REPO2" "$REPO3")
+
+ARRAY_REPO=("$REPO1" "$REPO2")
+for REPO in "${ARRAY_REPO[@]}"; do   # The quotes are necessary here
+    echo "Repository: $REPO"
+    git clone $REPO
+done
