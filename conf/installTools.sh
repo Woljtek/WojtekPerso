4c1912df6f79af491f07e4feb8402ab63cc99650
Woljtek <fabien.craheix@gmail.com>
2017-05-20 17:19:36 +0200

    Add list of tools with automatic install

---
+++ conf/installTools.sh
@@ -0,0 +1,18 @@
+# Init: 20/05/2017
+# Author: F. CRHX
+# Synopsis: Install some tools ...
+ 
+# Video tools
+sudo apt install vlc
+
+# Pictures tools
+sudo apt install darktable
+
+# Desktop work tools
+sudo apt install libreoffice-writer
+
+# Coding tools
+sudo apt install dos2unix
+sudo apt install vim
+#sudo apt install notepad
+
