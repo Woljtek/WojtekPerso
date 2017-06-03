d589e23aa4778293258d22e73e53fa2fcd7ea537
Woljtek <fabien.craheix@gmail.com>
2017-06-03 19:26:13 +0200

    Add second disk to fstab

---
+++ conf/mount_bigDisk.sh
@@ -0,0 +1,14 @@
+# Init: 03/0/2017
+# Author: F. CRHX
+# Synopsis: Mount the second disk on /media/bigDisk
+# Note UUID has to be manually update
+
+
+UUID=558922f8-16b2-4650-9966-cf3e6f46938b
+
+
+sudo mkdir /media/bigDisk
+sudo echo "$UUID /media/ddi            ext4    defaults        0       2" >> /etc/fstab
+sudo chown fchx /media/bigDisk
+
+# Computer must be rebooted 
