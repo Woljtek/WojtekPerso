0c51eb7d91acb3b9663133732ba815b23b05c907
Woljtek <fabien.craheix@gmail.com>
2020-04-08 17:49:04 +0200

    Few improvement for saveDockerVarLibGrafana.sh. Test OK

--- grafana/saveDockerVarLibGrafana.sh
+++ grafana/saveDockerVarLibGrafana.sh
@@ -13,7 +13,7 @@
 
 # Default destination dir
 GRAFANA_DIR=/var/lib/grafana/
-DEST_DIR=/media/bigDisk/03-DEV/3-Docker/grafana-bind-mount2/$GRAFANA_DIR
+DEST_DIR=/media/bigDisk/03-DEV/3-Docker/grafana-bind-mount/$GRAFANA_DIR
 BLU='\033[34m'
 VIO='\033[35m'
 RED='\033[31m'
@@ -21,13 +21,17 @@ YEL='\033[33m'
 VER='\033[32m'
 ENDC='\033[0m'
 
-if [ $? -eq 1 ] ; then
+if [ $# -eq 1 ] ; then
   DEST_DIR=$1
+else
+  echo -e "Use default destination directory: $YEL$DEST_DIR$ENDC"
+
 fi 
+
 ID=`sudo docker ps | grep grafana | cut -d' ' -f1`
 
 if [ ! -d $DEST_DIR ] ; then
-  echo INFO, create destination directory:
+  echo -e "\nINFO, create destination directory:"
   mkdir -pv $DEST_DIR
   if [ ! -d $DEST_DIR ] ; then
     echo -e $RED"ERROR$ENDC, cannot create destination directory"
@@ -41,9 +45,9 @@ if [ -z $ID ] ; then
 else
   echo -e "Save $YEL/var/lib/grafana$ENDC data of $YEL$ID$ENDC container in $YEL$DEST_DIR$ENDC directory"
 fi
+echo "CMD: sudo docker cp  $ID:$GRAFANA_DIR $DEST_DIR"
+sudo docker cp  $ID:$GRAFANA_DIR $DEST_DIR
 
-sudo docker cp  $ID:/var/lib/grafana $DEST_DIR
-
-echo "\nINFO, Display destination directory"
+echo -e "\nINFO, Display destination directory"
 ls -lhR --color $DEST_DIR
 exit 0
